4774a3a463a7bf70dec6f2bb33bdd6e1c41b9641
BeletteFurtive <francois.lallemand@openmailbox.org>
2015-02-20 20:56:09 +0100

    ajout d'un bouton permettant de relancer awesome

--- Interface.py
+++ Interface.py
@@ -2,6 +2,8 @@
 import argparse
 from gi.repository import Gtk
 from os import path
+import subprocess
+from subprocess import Popen
 import themeChooser
 
 class Handler:
@@ -14,8 +16,14 @@ class Handler:
 
     def on_comboboxAwesomeTheme_changed(self, widget):
         self.f.defineTheme(self.f.folderlist[int(widget.get_active_id())])
-        
 
+    def on_buttonRestartAwesome_clicked(self, widget):
+        #subprocess.call( ["echo", "awesome.restart()", "">>"", "caca"])
+        p1 = Popen(["echo", "awesome.restart()"], stdout=subprocess.PIPE)
+        p2 = Popen(["awesome-client"], stdin=p1.stdout, stdout=subprocess.PIPE)
+        p1.stdout.close()
+        output = p2.communicate()[0]
+        
 def main():
 
     f = themeChooser.FolderListing()
@@ -32,6 +40,10 @@ def main():
         i+=1
     
     comboboxAwesomeTheme.set_active(0)
+
+
+    button =  builder.get_object("button2")
+    
     
     window = builder.get_object("mainWindow")
     window.show_all()
